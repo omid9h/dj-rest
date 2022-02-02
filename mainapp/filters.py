935b401974d4aa343fe8f4029f4ba4b23851af27
omid9h <omid9h@gmail.com>
2022-02-02 23:59:19 +0330

    modify customer filter

--- mainapp/filters.py
+++ mainapp/filters.py
@@ -10,14 +10,9 @@ from mainapp.models.customer import Customer
 
 
 class CustomerFilter(FilterSet):
-    postpone_after = NumberFilter(field_name="postpone_date", lookup_expr="gt")
-
     class Meta:
         model = Customer
         fields = {
-            key: ["exact", "contains"]
-            for key in (
-                "name",
-                "email",
-            )
+            "name": ["exact", "contains"],
+            "email": ["exact"],
         }
